4c43fa20de926ecaa2daa157ca0cf1427cd2ca4c
Мій <admin@macbook-pro-igor.local>
2018-10-30 19:14:08 +0200

    Initial Commit

---
+++ Homework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework
+//
+//  Created by Мостовий Ігор on 30.10.18.
+//  Copyright © 2018 Мостовий Ігор. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
